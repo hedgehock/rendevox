ca40a78540cadc73c7c06ad66694c34767c6926a
hedge-mac <hedge@hedge.com>
2023-02-03 08:17:26 +0100

    more opengl comments

--- rendevox/src/openglWindow.c
+++ rendevox/src/openglWindow.c
@@ -104,22 +104,27 @@ void createOpenglWindow(window window)
             0.0f,  0.5f, 0.0f  // top
     };
 
+    // Generate Vertex array and Vertex buffer
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
 
+    // Bind Vertex Array
     glBindVertexArray(VAO);
 
+    // Pass buffer data
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // Shader attributes
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    // Unbind Vertex Array
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     glBindVertexArray(0);
 
-    // uncomment this call to draw in wireframe polygons.
+    // Wireframe
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 }
 
